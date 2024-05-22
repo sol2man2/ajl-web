2fda65a42c0ad409e0c4b3bf2ac62c018d2f1222
KeunChang SONG <sol2man2@gmail.com>
2024-05-23 00:07:53 +0900

    첫 화면 구성

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-[1980px] h-[10000px] pb-[8277px] bg-white flex-col justify-start items-start inline-flex">
+    <div class="self-stretch h-[94px] pl-[455px] pr-[1343px] pt-[22px] pb-3 bg-purple-300 justify-start items-center inline-flex">
+        <div class="self-stretch justify-start items-center gap-[241px] inline-flex">
+			<div class="w-[182px] h-[60px] justify-center items-center gap-1.5 flex">
+				<img class="w-[250px] h-[92px]" src="large 애잘린.png" />
+			</div>
+        </div>
+    </div>
+
+    <div class="self-stretch h-[600px] pl-[463px] pr-[464px] py-[19px] bg-purple-200 justify-center items-center inline-flex">
+        <div class="self-stretch justify-start items-start gap-10 inline-flex">
+            <div class="w-[408px] flex-col justify-start items-start gap-[72px] inline-flex">
+                <div class="text-black text-[80px] font-normal font-['Inter'] leading-[80px]">Value<br/>Outcome</div>
+                <div class="w-[382px] h-[308px] text-black text-[28px] font-normal font-['Inter'] leading-7">애잘린이 추구하는 것은 <br/>고객 가치를 담은<br/>프로덕트와 서비스를 창출하는<br/>행복한 문화이다.</div>
+            </div>
+            <img class="w-[605px] h-[562px]" src="collaboration development.jpeg" />
+        </div>
+    </div>
+    <div class="self-stretch h-[600px] pl-[463px] pr-[464px] pt-[19px] pb-[18px] bg-purple-300 justify-center items-center inline-flex">
+        <div class="grow shrink basis-0 self-stretch justify-start items-start gap-20 inline-flex">
+            <img class="w-[367px] h-[563px]" src="sol2man2-kakaobrain.jpeg" />
+            <div class="grow shrink basis-0 flex-col justify-start items-start gap-[72px] inline-flex">
+                <div class="self-stretch text-black text-[80px] font-normal font-['Inter'] leading-[80px]">Agile Coach<br/>송근창</div>
+                <div class="self-stretch h-[308px] text-black text-[28px] font-normal font-['Inter'] leading-[33.60px]">LG전자에서 혁신 활동과 SW 개발을 컨설팅하고 코칭하고 RedHat에서 Open Innovation Labs 솔루션에 Agile Engagement Lead로 고객사의 가치 창출을 지원했으며 카카오브래인에서 Agile Coach로 활동하고 있다.<br/>특히 Jeff Sutherland 박사님의 True Scrum을 추구하여 복잡계 시스템을 게임의 룰에는 간단하게 접근하고 게임의 정신에는 깊이 있게 다룬다.</div>
+            </div>
+        </div>
+    </div>
+    <div class="self-stretch h-[429px] pl-[463px] pr-[469px] py-[19px] bg-purple-200 justify-start items-center inline-flex">
+        <div class="self-stretch justify-start items-start gap-[89px] inline-flex">
+            <div class="w-[754px] h-[391px] justify-start items-start gap-10 flex">
+                <div class="grow shrink basis-0 self-stretch flex-col justify-start items-start gap-10 inline-flex">
+                    <div class="self-stretch h-28 text-black text-[80px] font-normal font-['Inter'] leading-[80px]">RSMPO</div>
+                    <div class="self-stretch grow shrink basis-0 text-black text-[28px] font-normal font-['Inter'] leading-[33.60px]">Scruminc의 Registered Scrum Master 과정과 Registered Product Owner 인증 과정을 하나로 통합한 교육으로 Scrum Master의 역할과 Product Owner의 역할을 체험하고 RSM과 RPO의 듀얼 자격 인증을 취득한다.</div>
+                </div>
+            </div>
+            <div class="flex-col justify-start items-start gap-[25px] inline-flex">
+                <img class="w-[205px] h-[183px]" src="SM-Badge.webp" />
+                <img class="w-[205px] h-[183px]" src="PO-Badge.webp" />
+            </div>
+        </div>
+    </div>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
